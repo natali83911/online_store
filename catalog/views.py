ce93b6f5d3a9fe40e5830ac1d32c4068f976e509
Natalya Sorokina <natali83911@gmail.com>
2025-08-14 23:06:12 +0300

    5 modified files

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
 from catalog.models import Product
-from django.views.generic import TemplateView, ListView, DetailView
-
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from catalog.forms import ProductForm
 
 
 class HomeView(TemplateView):
@@ -22,3 +23,21 @@ class ProductDetailView(DetailView):
     model = Product
     template_name = 'catalog/products_detail.html'
     context_object_name = 'product'
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:products_list')
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:products_list')
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'catalog/product_confirm_delete.html'
+    success_url = reverse_lazy('catalog:products_list')
