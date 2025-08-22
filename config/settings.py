409a1ba4dcd0c5abc27da0ba2ede41ef7c270eea
Natalya Sorokina <natali83911@gmail.com>
2025-08-22 23:50:57 +0300

    2 modified files

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
     "catalog",
     "blogs",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -123,16 +124,15 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'users.CustomUser'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(("catalog.urls", "catalog"), namespace="catalog")),
     path('blogs/', include('blogs.urls', namespace='blogs')),
+    path('users/', include('users.urls', namespace='users')),
 ]
 
 
